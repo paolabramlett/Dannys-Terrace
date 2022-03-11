0ea3661c5bd5a566e03a31861c0d667b611f8e02
paolabramlett <76532437+paolabramlett@users.noreply.github.com>
2022-03-11 16:09:55 -0600

    giphys

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl)
+})
